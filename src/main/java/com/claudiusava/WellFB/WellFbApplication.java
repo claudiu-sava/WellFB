631a9521f8820a30af14c30b78be5ea4603c6845
Claudiu Sava <57670211+claudiu-sava@users.noreply.github.com>
2023-10-30 15:12:05 +0100

    add static paths

--- src/main/java/com/claudiusava/WellFB/WellFbApplication.java
+++ src/main/java/com/claudiusava/WellFB/WellFbApplication.java
@@ -1,24 +1,20 @@
 package com.claudiusava.WellFB;
 
-import com.claudiusava.WellFB.model.Role;
-import com.claudiusava.WellFB.repository.RoleRepository;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class WellFbApplication {
 
 	public static String UPLOAD_DIRECTORY;
 	public static String UPLOAD_BASE;
-	public static String STATIC_RESOURCES;
+	public static String DRAWABLE_RESOURCES;
 	public static String CSS_RESOURCES;
 
 	public static void main(String[] args) {
 		UPLOAD_BASE = "/uploads/";
 		UPLOAD_DIRECTORY = System.getProperty("user.dir") + "/src/main/resources" + UPLOAD_BASE;
-		STATIC_RESOURCES = System.getProperty("user.dir") + "/src/main/resources/static/drawable/";
+		DRAWABLE_RESOURCES = System.getProperty("user.dir") + "/src/main/resources/static/drawable/";
 		CSS_RESOURCES = System.getProperty("user.dir") + "/src/main/resources/static/css/";
 
 		SpringApplication.run(WellFbApplication.class, args);
