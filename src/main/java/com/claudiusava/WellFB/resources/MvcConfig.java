675655aed021503fe3207c3c085b00b7e61a1fa6
Claudiu Sava <57670211+claudiu-sava@users.noreply.github.com>
2023-10-30 15:11:51 +0100

    Update MvcConfig.java

--- src/main/java/com/claudiusava/WellFB/resources/MvcConfig.java
+++ src/main/java/com/claudiusava/WellFB/resources/MvcConfig.java
@@ -14,7 +14,7 @@ public class MvcConfig implements WebMvcConfigurer {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/uploads/**", "/drawable/**", "/css/**")
-                .addResourceLocations("file:" + UPLOAD_DIRECTORY, "file:" + STATIC_RESOURCES, "file:" + CSS_RESOURCES)
+                .addResourceLocations("file:" + UPLOAD_DIRECTORY, "file:" + DRAWABLE_RESOURCES, "file:" + CSS_RESOURCES)
                 .setCachePeriod(0)
                 .resourceChain(true)
                 .addResolver(new PathResourceResolver());
