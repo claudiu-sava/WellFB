61c968182c783d52a3e0a8164a78cdcf9fdfe311
Claudiu Sava <57670211+claudiu-sava@users.noreply.github.com>
2023-10-30 13:36:32 +0100

    remove unused imports

--- src/main/java/com/claudiusava/WellFB/controller/LoginController.java
+++ src/main/java/com/claudiusava/WellFB/controller/LoginController.java
@@ -6,11 +6,8 @@ import org.springframework.security.web.WebAttributes;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.naming.AuthenticationException;
-import java.util.Optional;
 
 @Controller
 public class LoginController {
