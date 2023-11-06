0c3abd993d228a4d16c63d8f5a06d406d99aaaeb
Claudiu Sava <57670211+claudiu-sava@users.noreply.github.com>
2023-11-06 11:27:18 +0100

    random users show

--- src/main/java/com/claudiusava/WellFB/repository/UserRepository.java
+++ src/main/java/com/claudiusava/WellFB/repository/UserRepository.java
@@ -3,9 +3,10 @@ package com.claudiusava.WellFB.repository;
 import com.claudiusava.WellFB.model.User;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface UserRepository extends CrudRepository<User, Integer> {
     Optional<User> findByUsername(String username);
-    Iterable<User> findFirst10By();
+    List<User> findAllByOrderByUsernameAsc();
 }
