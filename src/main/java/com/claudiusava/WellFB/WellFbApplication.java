e4259d8d71ab206853df75cec5dd24e6346b53b2
Claudiu Sava <57670211+claudiu-sava@users.noreply.github.com>
2023-12-07 08:37:35 +0100

    add hq media folder

--- src/main/java/com/claudiusava/WellFB/WellFbApplication.java
+++ src/main/java/com/claudiusava/WellFB/WellFbApplication.java
@@ -13,8 +13,16 @@ public class WellFbApplication {
 	public static String JS_RESOURCES;
 	public static String AVATAR_DIRECTORY;
 	public static String AVATAR_BASE;
+	public static String HQ_UPLOAD_BASE;
+	public static String HQ_UPLOAD_DIRECTORY;
+	public static String TEMPLATE_BASE;
+	public static String TEMPLATE_DIRECTORY;
 
 	public static void main(String[] args) {
+		TEMPLATE_BASE = "/templates/fragments/";
+		TEMPLATE_DIRECTORY = System.getProperty("user.dir") + "/src/main/resources" + TEMPLATE_BASE;
+		HQ_UPLOAD_BASE = "/hq/";
+		HQ_UPLOAD_DIRECTORY = System.getProperty("user.dir") + "/src/main/resources" + HQ_UPLOAD_BASE;
 		UPLOAD_BASE = "/uploads/";
 		UPLOAD_DIRECTORY = System.getProperty("user.dir") + "/src/main/resources" + UPLOAD_BASE;
 		DRAWABLE_RESOURCES = System.getProperty("user.dir") + "/src/main/resources/static/drawable/";
